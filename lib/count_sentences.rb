6f76b580a1fb7bcfd3d076300afed706245be04c
Helen Du <helendu.614@gmail.com>
2018-01-11 23:55:05 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,12 +2,22 @@ require 'pry'
 
 class String
 
+#returns true if the string youa re calling this method on ends in a period
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
+#returns true if the string you are calling this method on ends in question mark
   def question?
-
+    if self.end_with("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
